2a567cdd43757aba7f30f6361dc2535049a67758
Augusto Almeida <66666582+AuggieAlmeida@users.noreply.github.com>
2021-09-07 11:42:05 -0300

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -17,21 +17,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        move();
+        
+        //anim.SetBool("isMoving", (Mathf.Abs(moveInput.x) != 0 || Mathf.Abs(moveInput.y) != 0));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        // Morrer quando encostar em inimigo
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject, 0.5f);
+        }
+        
+    }
+    private void Move(){
+        // Movimentação do jogador
         moveInput.x = Input.GetAxis("Horizontal");
         moveInput.y = Input.GetAxis("Vertical");
 
         transform.Translate(moveInput * Time.deltaTime * moveSpeed);
 
         anim.SetBool("isMoving", (moveInput.x != 0 || moveInput.y != 0));
-        
-        //anim.SetBool("isMoving", (Mathf.Abs(moveInput.x) != 0 || Mathf.Abs(moveInput.y) != 0));
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision){
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            Destroy(gameObject, 0.5f);
-        }
-        
     }
 }
