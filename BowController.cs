abeb0e1e1dc69b9bc778b85947bcb62acd5dd152
AuggieAlmeida <66666582+SiegBertT@users.noreply.github.com>
2021-09-07 08:34:08 -0300

    Testes de movimentação e jogabilidade em C#

---
+++ BowController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowController : MonoBehaviour
+{
+    SpriteRenderer sprite;
+    AudioSource shootFx;
+
+    public GameObject arrow;
+    public Transform arrowSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        shootFx = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Aim();
+        Shoot();
+    }
+
+    void Aim() 
+    {
+    
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        sprite.flipY = (mousePos.x < screenPoint.x);
+
+        /*if (mousePos.x < screenPoint.x) {
+            sprite.flipY = true;
+        else 
+            sprite.flipY = false;*/
+    }
+
+    void Shoot() 
+    {
+        if (Input.GetButtonDown("Fire1")) 
+        {
+            Instantiate(arrow, arrowSpawn.position, transform.rotation);
+            shootFx.Play();
+        }
+
+    }
+}
---
+++ EnemyController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] AudioClip deathFx;
+
+    GameObject player;
+    Animator anim;
+    AudioSource enemyFx;
+    bool isAlive = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponentInChildren<Animator>();    
+        enemyFx = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null && isAlive) {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Hit")) {
+            anim.SetTrigger("Dead");
+            isAlive = false;
+            enemyFx.PlayOneShot(deathFx);
+            Destroy(gameObject, 0.3f);
+        }
+    }
+
+}
